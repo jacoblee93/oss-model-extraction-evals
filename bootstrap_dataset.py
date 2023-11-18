5b97e1013bf3262529a9e70f44e1989876560ff8
jacoblee93 <jacoblee93@gmail.com>
2023-11-18 02:44:06 -0800

    Initial commit

---
+++ bootstrap_dataset.py
@@ -0,0 +1,66 @@
+import os
+import glob
+
+from typing import Optional, List
+from enum import Enum
+
+from langchain.pydantic_v1 import BaseModel, Field
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.chains.openai_functions import (
+    convert_to_openai_function,
+    get_openai_output_parser,
+)
+
+os.environ["LANGCHAIN_API_KEY"] = ""
+os.environ["LANGCHAIN_SESSION"] = "extraction-dataset-gathering"
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+
+os.environ["OPENAI_API_KEY"] = ""
+
+class ToneEnum(str, Enum):
+    positive = "positive"
+    negative = "negative"
+
+
+class Email(BaseModel):
+    """Relevant information about an email."""
+
+    sender: Optional[str] = Field(None, description="The sender's name, if available")
+    sender_phone_number: Optional[str] = Field(None, description="The sender's phone number, if available")
+    sender_address: Optional[str] = Field(None, description="The sender's address, if available")
+    action_items: List[str] = Field(..., description="A list of action items requested by the email")
+    topic: str = Field(..., description="High level description of what the email is about")
+    tone: ToneEnum = Field(..., description="The tone of the email.")
+    
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are an expert researcher."),
+        (
+            "human",
+            "What can you tell me about the following email? Make sure to answer in the correct format: {email}",
+        ),
+    ]
+)
+
+openai_functions = [convert_to_openai_function(Email)]
+llm_kwargs = {
+    "functions": openai_functions,
+    "function_call": {"name": openai_functions[0]["name"]}
+}
+
+llm = ChatOpenAI(temperature=0, model="gpt-4-1106-preview")
+
+output_parser = get_openai_output_parser([Email])
+extraction_chain = prompt | llm.bind(**llm_kwargs) | output_parser
+
+files = glob.glob('./formatted/*')
+
+for file in files:
+    with open(file, 'r') as f:
+        content = f.read()
+        print(file)
+        extraction_chain.invoke({
+            "email": content
+        })
---
+++ format.py
@@ -0,0 +1,16 @@
+import mailbox
+from langchain.schema import Document
+from langchain.document_transformers import Html2TextTransformer
+ 
+# Replace 'input.mbox' with your MBOX file name
+mbox_file = '../Mail/Spam.mbox'
+ 
+mbox = mailbox.mbox(mbox_file)
+html2text = Html2TextTransformer()
+ 
+for i, message in enumerate(mbox):
+    print(i)
+    with open(f'./formatted/email_{i}.eml', 'wb') as f:
+        raw_doc = Document(page_content=message.as_string())
+        transformed_docs = html2text.transform_documents([raw_doc])
+        f.write(transformed_docs[0].page_content.replace("jacoblee93", "jacob").replace("= ", "").replace("=E2=80=94", "").encode("utf-8"))
